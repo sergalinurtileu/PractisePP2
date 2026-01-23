6577062177bbc738f0b6fcfaed1401f7f097ae34
Nurtileu <sergalinurtileu93@gmail.com>
2026-01-23 17:09:14 +0500

    done exvariables.py

---
+++ preactise1/Variables.py
@@ -0,0 +1,26 @@
+a = 5
+a = "NUrtileu"
+print(type(a))
+myvar = "John"
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+fruits = ["apple", "banana", "cherry"]
+e, d, c = fruits
+print(e)
+print(d)
+print(c)
+
+
+
+
+q= "awesome"
+
+def myfunc():
+    global q
+    q = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
