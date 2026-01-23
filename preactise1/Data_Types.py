e6ebf2deac1d01ac1e04387ecb87363d81049c35
Nurtileu <sergalinurtileu93@gmail.com>
2026-01-23 16:58:04 +0500

    done ex data_type.py

---
+++ preactise1/Data_Types.py
@@ -0,0 +1,16 @@
+a = "Hello World"
+b=20.5
+c=["microsoft", "tiktok", "iphone"]
+d=range(3)
+e=True
+f= b"Hello"
+g=None
+h=20
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
