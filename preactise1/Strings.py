02dadc84f1687bb6d54c5b92acdeb18823cd3c74
Nurtileu <sergalinurtileu93@gmail.com>
2026-01-23 17:14:54 +0500

    done ex string.py

---
+++ preactise1/Strings.py
@@ -0,0 +1,14 @@
+a = "Hello, my teacher!"
+print(a)
+print(a[:5])
+print(a.lower())
+print(a.strip())
+print(a.replace("H", "J"))
+print(a.split(","))
+b="hello"
+c='world'
+print(b +" " + c )
+age = 18
+txt = f"My name is Nurtileu, I am {age}"
+print(txt)
+txt = "We are the so-called \"KAZAKH nation\" from the central Asia."
