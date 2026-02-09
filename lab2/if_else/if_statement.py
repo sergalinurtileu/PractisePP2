76b57147d04eb14b7c1a168c46a7c9f73cd22661
Nurtileu <sergalinurtileu93@gmail.com>
2026-02-09 23:29:42 +0500

    New lab3

--- lab2/if_else/if_statement.py
+++ lab2/if_else/if_statement.py
@@ -1,22 +1,31 @@
-#1 example
+#1 
 a = 100
 b = 200
 if b > a:
   print("b is greater than a")
 
-#2 example
+#2 
 number = 123
 if number > 0:
   print("The number is positive")
 
-#3 example
+#3 
 age = 18
 if age >= 18:
   print("You are an adult")
   print("You can vote")
   print("You have full legal rights")
-
-#4 example
+  
+#4 
 is_logged_in = True
 if is_logged_in:
   print("Welcome back!")
+
+#5 
+qq=int(input("Enter a number:"))
+if qq%2==0:
+    print("Even number")
+
+
+
+ 
